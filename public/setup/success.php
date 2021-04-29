9e5fc43735b7ac1ad8b7d3b47bef66e32c4fc3fc
Glafuski <glafuski@gmail.com>
2021-04-29 03:31:03 +0300

    Lisättiin asennustiedosto.

---
+++ public/setup/success.php
@@ -0,0 +1,4 @@
+<div class="column">
+<h1>You have succesfully installed JJMVC</h1>
+<p>After this request JJMVC should remove setup folder automatically. However you should check that setup folder is deleted from public folder. If setup folder is not removed we recommend to remove it manually as it could be security issue.</p>
+</div>

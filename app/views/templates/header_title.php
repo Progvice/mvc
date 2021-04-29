8ce371e91b86eb6e8a8e764669207a26a89d0b80
Glafuski <glafuski@gmail.com>
2021-04-29 03:49:14 +0300

    Pieni muutos headeriin

--- app/views/templates/header_title.php
+++ app/views/templates/header_title.php
@@ -3,9 +3,10 @@ namespace Core\App\Template;
 class header_title extends \Core\App\Template {
     protected $template;
     public function Load($values) {
+        $name = CONFIG['server']['name'];
         $this->template = <<<EOT
             <div class="header_title">
-                <a href="/"><h2>Jani Juuso</h2></a>
+                <a href="/"><h2>{$name}</h2></a>
             </div>
         EOT;
         return $this->template;

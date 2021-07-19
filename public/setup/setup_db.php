6f15eb99716bf98185f237f524316ea56a498de4
Glafuski <glafuski@gmail.com>
2021-07-20 01:28:09 +0300

    Removed gitignore and setup folder is back.

---
+++ public/setup/setup_db.php
@@ -0,0 +1,7 @@
+<form method="post" action="/setup/post/send_db.php">
+    <input type="text" name="dbname" placeholder="Database name" required/><br>
+    <input type="text" name="host" placeholder="Host (ex. domain.com)" required/><br>
+    <input type="text" name="username" placeholder="Username" required/><br>
+    <input type="password" name="password" placeholder="Password"/><br>
+    <button type="submit">Set configuration</button>
+</form>
